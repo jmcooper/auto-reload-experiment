8864b43d100add6753d438fba56783fa9627dbc7
Jim Cooper <jim@cooperplanet.com>
2019-08-22 08:24:47 -0600

    Add error handling and cleanup script file

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-"use strict";
-clearDisplay();
-(function() {
-  let cat = { name: "Fluffy" };
+'use strict';
 
-  display(cat);
-})();
+// var cat = {
+//   name: 'Fluffy'
+// };
+
+// var dog = {
+//   name: 'Spot'
+// };
+
+throw new Error('wat?');
+
+// display(dog, { test: 2 });
+
+// display({ test: 3 });
